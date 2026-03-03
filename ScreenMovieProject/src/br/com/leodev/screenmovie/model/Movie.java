a628ed6972875ba48b67a358dc2e8b5a62b17371
leonardoteles31 <leo.teleso.10@hotmail.com>
2026-03-02 21:51:54 -0300

    ScreenMovie updated, module names changed n create module ChallengeMod3POO

--- ScreenMovieProject/src/br/com/leodev/screenmovie/model/Movie.java
+++ ScreenMovieProject/src/br/com/leodev/screenmovie/model/Movie.java
@@ -1,68 +1,13 @@
 package br.com.leodev.screenmovie.model;
 
-public class Movie {
-    private String title;
-    private int releaseYear;
-    private boolean includedInPlan;
-    private double rating;
-    private int totalRatings;
-    private int durationInMinutes;
+public class Movie extends Title {
+    private String director;
 
-    public String getTitle() {
-        return title;
+    public String getDirector() {
+        return director;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getReleaseYear() {
-        return releaseYear;
-    }
-
-    public void setReleaseYear(int releaseYear) {
-        this.releaseYear = releaseYear;
-    }
-
-    public boolean isIncludedInPlan() {
-        return includedInPlan;
-    }
-
-    public void setIncludedInPlan(boolean includedInPlan) {
-        this.includedInPlan = includedInPlan;
-    }
-
-    public double getRating() {
-        return rating;
-    }
-
-    public int getTotalRatings() {
-        return totalRatings;
-    }
-
-    public int getDurationInMinutes() {
-        return durationInMinutes;
-    }
-
-    public void setDurationInMinutes(int durationInMinutes) {
-        this.durationInMinutes = durationInMinutes;
-    }
-
-    public void showDetails() {
-        System.out.println("Title: " + this.title);
-        System.out.println("Release Year: " + this.releaseYear);
-        System.out.println("Included in Plan: " + this.includedInPlan);
-        System.out.println("Rating: " + this.rating);
-        System.out.println("Total Ratings: " + this.totalRatings);
-        System.out.println("Duration (minutes): " + this.durationInMinutes);
-    }
-
-    public void rates(double rate) {
-        rating += rate;
-        totalRatings++;
-    }
-
-    public double rateAverage() {
-        return rating / totalRatings;
+    public void setDirector(String director) {
+        this.director = director;
     }
 }
